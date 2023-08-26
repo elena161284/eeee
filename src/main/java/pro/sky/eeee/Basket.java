0de0748404f3458da3d75625ccad5a18210d8d51
Elena <elena161284@mail.ru>
2023-08-26 22:14:12 +0300

    commit finish

--- src/main/java/pro/sky/eeee/Basket.java
+++ src/main/java/pro/sky/eeee/Basket.java
@@ -1,5 +1,23 @@
 package pro.sky.eeee;
 
-public class Basket {
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.SessionScope;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+@Service
+@SessionScope
+public class Basket {
+    private final List<Integer> items = new ArrayList<>();
+    public  void add(Integer[] ids) {
+        for (Integer id:ids) {
+            items.add(id);
+        }
+    }
+
+    public List<Integer> getAll() {
+        return Collections.unmodifiableList(items);
+    }
 }
